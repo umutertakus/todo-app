4b9dc7e877b2311b753933bb8303d07548c18f08
umutertakus <umutertakus@hotmail.com>
2022-09-08 14:35:27 +0300

    Project setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from "@emotion/styled";
+import { Box } from "@mui/system";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Wrapper>
+      Todo App
+    </Wrapper>
   );
 }
 
 export default App;
+
+const Wrapper = styled(Box)(() => ({
+  padding: 0,
+  margin: 0,
+  height: "100vh",
+  width: "100vw",
+  display: "flex",
+}));
